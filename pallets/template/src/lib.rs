9b9428765a717c85858fe43ea42ad69ed76c67b0
lzw65 <372208209@qq.com>
2020-10-22 09:38:08 +0800

    add MyStruct Storage

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -6,12 +6,19 @@
 
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
+use frame_support::codec::{Encode, Decode};
 
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct MyStruct {
+    number: u32,
+    option: Option<u32>,
+}
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -29,6 +36,7 @@ decl_storage! {
 		// Learn more about declaring storage items:
 		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
 		Something get(fn something): Option<u32>;
+		MyStructNumber get(fn structnumberinfo): Option<MyStruct>;
 	}
 }
 
@@ -39,6 +47,7 @@ decl_event!(
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, AccountId),
+		MyStructNumberStored(u32, Option<u32>, AccountId),
 	}
 );
 
@@ -77,6 +86,25 @@ decl_module! {
 
 			// Emit an event.
 			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// Return a successful DispatchResult
+			Ok(())
+		}
+
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn do_structnumberinfo(origin, number: u32, option: Option<u32>) -> dispatch::DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			MyStructNumber::put(MyStruct{
+				number,
+				option
+			});
+
+			// Emit an event.
+			Self::deposit_event(RawEvent::MyStructNumberStored(number, option, who));
 			// Return a successful DispatchResult
 			Ok(())
 		}
